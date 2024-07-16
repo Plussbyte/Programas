556ac5bbdc674d39ca0a78216dc654bb518e92ba
Plussbyte <samuel.estrada139@pascualbravo.edu.co>
2024-07-16 10:16:07 -0500

    Actualización 1.6

--- Login Program/Logic/logicOfCheckIn.mjs
+++ Login Program/Logic/logicOfCheckIn.mjs
@@ -7,7 +7,8 @@ const interfaceLogin = document.getElementById('loginInterfaceLogic')
 // ↓ Inputs del login
 const inputDocument = document.getElementById('documentLognLogic')
 // ↓ Inputs del registro
-const documentCheckInLogic = document.getElementById('documentCheckInLogic')
+const documentCheckIn = document.getElementById('documentCheckInLogic')
+const newPasswordCheckIn =document.getElementById('newPasswordCheckInLogic')
 
 
 // ↓ Errores del login
@@ -16,6 +17,8 @@ const errorFiveNum = document.getElementById('errorFiveNumLognLogic')
 // ↓ Errores del registro
 const errorNumbersDocument = document.getElementById('numbersErrorCheckInLogic')
 const errorCharactersDocument = document.getElementById('charactersErrorCheckInLogic')
+const errorCharactersPassword = document.getElementById('passwordErrorCheckInLogic')
+const errorNewPassword = document.getElementById('ErrorNewPasswordCheckInLogic')
 
 
 const buttonCheckIn = document.getElementById('checkInLognLogic')
@@ -40,24 +43,41 @@ export function enterCheckIn(){
     /* ↓ Manejo del evento click del boton validar de registrarse */
     acceptButtonCheckIn.addEventListener('click',()=>{
         let iteratorNoLettersCheckIn = 0
-        iteratorNoLettersCheckIn = noLetters(documentCheckInLogic,iteratorNoLettersCheckIn)
+        let iteradorPasswordErrors = 0
 
-        console.log(documentCheckInLogic.value.length)
-        console.log(iteratorNoLettersCheckIn)
+        /* ↓ Manejo de errores del input documento */
+        iteratorNoLettersCheckIn = noLetters(documentCheckIn,iteratorNoLettersCheckIn)
 
-        if(documentCheckInLogic.value.length < 6){
+        if(documentCheckIn.value.length < 6){
             errorCharactersDocument.style.display = 'flex'
             errorNumbersDocument.style.display = 'none'
 
-        } else if(iteratorNoLettersCheckIn !== documentCheckInLogic.value.length){
+        } else if(iteratorNoLettersCheckIn !== documentCheckIn.value.length){
             errorNumbersDocument.style.display = 'flex'
             errorCharactersDocument.style.display = 'none'
             
         } else{
             errorCharactersDocument.style.display = 'none'
             errorNumbersDocument.style.display = 'none'
+            iteradorPasswordErrors = 1
         }
         //NOTA: Para usar el valor final (Return) de una funcion por fuera de la función hay que encerrarla en una variable
+        /* ↑ Manejo de errores del input documento */
+
+
+        /* ↓ Manejo de errores de los inputs de contraseñas */
+        console.log(newPasswordCheckIn.value)
+        if(iteradorPasswordErrors === 1){
+
+            if(newPasswordCheckIn.value = ''){
+                errorNewPassword.style.display = 'flex'
+                errorCharactersPassword.style.display = 'none'
+            }else if(newPasswordCheckIn.value.length < 5){
+                errorCharactersPassword.style.display = 'flex'
+                errorNewPassword.style.display = 'none'
+            }
+        } else{}
+        /* ↑ Manejo de errores de los inputs de contraseñas */
     })  
 
     /* ↑ Manejo del evento click del boton validar de registrarse */
@@ -69,7 +89,7 @@ export function enterCheckIn(){
         interfaceCheckIn.style.display = 'none'
         errorCharactersDocument.style.display = 'none'
         errorNumbersDocument.style.display = 'none'
-        documentCheckInLogic.value = ''
+        documentCheckIn.value = ''
     })
 
     /* ↑ Manejo del evento click del boton Cancelar de registrarse */
